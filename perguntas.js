bbe8e0dae51d1e83b69a3b88f44cf6372d6f98fa
julianawieczorkiewicz <141339764+julianawieczorkiewicz@users.noreply.github.com>
2024-11-07 20:14:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Futebol Geral',
+    'Qual o maior campeão inglês?',
+    'Manchester United (68)'
+)
+
+criaCartao(
+    'Futebol Geral',
+    'Qual foi o time campeão da Champions League 2017/2018?',
+    'Real Madrid'
+)
+
+criaCartao(
+    'Copa do Mundo',
+    'Qual é a seleção mais vezes campeã da Copas do Mundo?',
+    'Seleção Brasileira (5)'
+)
+
+criaCartao(
+    'Futebol Geral',
+    'Qual é o maior campeão do Brasileirão?',
+    'Palmeiras (12)'
+)
+
+criaCartao(
+    'Futebol Geral',
+    'Por quais times Lionel Messi passou?',
+    'Barcelona, Paris Saint-Germain (PSG), Inter Miami.'
+)
+
+criaCartao(
+    'Futebol Geral',
+    'Quem é o maior artilheiro do futebol em jogos oficiais?',
+    'Cristiano Ronaldo (908)'
+)
+
+criaCartao(
+    'Copa do Mundo',
+    'Quantas Copas do Mundo Pelé conquistou?',
+    '3 (1958, 1962, 1970)'
+)
